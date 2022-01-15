b1920879c016fe0b8530aab0a3d76796c4976941
Bhargab <bhargabnath691@gmail.com>
2022-01-14 21:33:43 -0800

    day 4 for milestone3 done

--- Microsoft/Ques12.cpp
+++ Microsoft/Ques12.cpp
@@ -1,27 +1,29 @@
 // https://practice.geeksforgeeks.org/problems/find-all-four-sum-numbers1732/1
 
-class Solution{
-    public:
+class Solution
+{
+public:
     // arr[] : int input array of integers
     // k : the quadruple sum required
-    vector<vector<int> > fourSum(vector<int> &arr, int k) {
+    vector<vector<int>> fourSum(vector<int> &arr, int k)
+    {
         // Your code goes here
         int n = arr.size();
         sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
-        set <vector<int>> s;
-        for (int i=0; i<n; i++)
+        set<vector<int>> s;
+        for (int i = 0; i < n; i++)
         {
-            for (int j=i+1; j<n; j++)
+            for (int j = i + 1; j < n; j++)
             {
-                int start = j+1;
-                int end = n-1;
+                int start = j + 1;
+                int end = n - 1;
                 while (start < end)
                 {
-                    int sum = arr[i]+arr[j]+arr[start]+arr[end];
+                    int sum = arr[i] + arr[j] + arr[start] + arr[end];
                     if (sum == k)
                     {
-                        s.insert({arr[i],arr[j],arr[start],arr[end]});
+                        s.insert({arr[i], arr[j], arr[start], arr[end]});
                         start++;
                         end--;
                     }
@@ -29,14 +31,14 @@ class Solution{
                     {
                         end--;
                     }
-                    else 
+                    else
                     {
                         start++;
                     }
                 }
             }
         }
-        for(auto i:s)
+        for (auto i : s)
         {
             ans.push_back(i);
         }
